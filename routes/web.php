234cd3e384297f4db1fdf7d42c4b0d11bb94bdf8
waleed_tarazi <waleedaltarazy@gmail.com>
2023-02-13 16:56:28 +0300

    adding the authentication

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 //Route::get('/user{id}',[UserController::class, 'show']);
 
 Route::get('/posts',[PostController::class,'index']);
+
+Auth::routes();
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
